868aba357599f531cb161489380525e7f636827a
delmito <delmo.hf@gmail.com>
2021-02-08 09:01:25 +0100

    f1

--- hello.php
+++ hello.php
@@ -1,5 +1,3 @@
 <?php
-echo 'Hello world! from kind.';
-echo 'Hello from mmm.';
-echo 'Hello from kind2.';
-echo 'Hello Yes! from kind2 again !!!!';
+echo 'Hello world! from kind';
+echo 'Hello from feat1';
