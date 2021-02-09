2af78926b61bb298e38ba3eacfeaba6849552051
KindoxPerseo <78487104+KindoxPerseo@users.noreply.github.com>
2021-02-09 22:04:32 +0100

    Again feature 2 (#5)
    * c1 on feat2
    
    * ccc3
    
    * kind A
    
    * hello from kind2
    
    * other hello from kind2
    
    * new file from kind2
    
    * update hello from kind2
    
    Co-authored-by: delmito <delmo.hf@gmail.com>

--- hello.php
+++ hello.php
@@ -1,5 +1,6 @@
 <?php
-echo 'Hello world! from kind';
-echo 'Hello from mmm';
-echo 'Hello from kind2';
+echo 'Hello world! from kind.';
+echo 'Hello from mmm.';
+echo 'Hello from kind2.';
+echo 'Hello Yes! from kind2 again';
 
